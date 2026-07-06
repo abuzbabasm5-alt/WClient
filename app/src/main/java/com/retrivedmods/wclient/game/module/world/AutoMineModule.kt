38e3c4e3b33c7bb9606e346bdbcc36faf2409129
abuzbabasm5-alt <abuzbabasm5@gmail.com>
2026-07-06 22:00:17 +0300

    Update AutoMineModule.kt

--- app/src/main/java/com/retrivedmods/wclient/game/module/world/AutoMineModule.kt
+++ app/src/main/java/com/retrivedmods/wclient/game/module/world/AutoMineModule.kt
@@ -1,30 +1,24 @@
-package com.retrivedmods.wclient.game.module.player
+package com.retrivedmods.wclient.game.module.world
 
+import com.retrivedmods.wclient.game.InterceptablePacket
 import com.retrivedmods.wclient.game.Module
 import com.retrivedmods.wclient.game.ModuleCategory
-import com.retrivedmods.wclient.game.entity.Player
+import org.cloudburstmc.protocol.bedrock.packet.PlayerActionPacket
 
-class AutoMineModule : Module("AutoMine", ModuleCategory.Misc) {
+class AutoMineModule : Module("auto_mine", ModuleCategory.World) {
 
-    enum class TargetOre(val blockName: String) {
-        All("all_ores"),
-        Diamond("diamond_ore"),
-        Gold("gold_ore"),
-        Iron("iron_ore"),
-        AncientDebris("ancient_debris"),
-        Coal("coal_ore")
-    }
+    private val speed by intValue("speed", 8, 1..20)
+    private var breakCounter = 0
 
-    private val targetOre by enumValue("target_ore", TargetOre.Diamond, TargetOre::class.java)
-    private val mineRadius by floatValue("radius", 4.5f, 1f..7f)
-    private val autoSwitchPickaxe by boolValue("auto_pickaxe", true)
-
-    override fun onEnabled() {}
-    override fun onDisabled() {}
-
-    fun onTick() {
-        if (!isEnabled || !isSessionCreated) return
-        val currentTarget = targetOre
-        val radius = mineRadius
+    override fun beforePacketBound(interceptablePacket: InterceptablePacket) {
+        if (!isEnabled) return
+        
+        val packet = interceptablePacket.packet
+        if (packet is PlayerActionPacket) {
+            breakCounter++
+            if (breakCounter >= (20 - speed)) {
+                breakCounter = 0
+            }
+        }
     }
 }
