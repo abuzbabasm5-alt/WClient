718613cd75065dc610f0685d015df2c533a87cbe
abuzbabasm5-alt <abuzbabasm5@gmail.com>
2026-07-06 21:59:57 +0300

    Update NoFallModule.kt

--- app/src/main/java/com/retrivedmods/wclient/game/module/world/NoFallModule.kt
+++ app/src/main/java/com/retrivedmods/wclient/game/module/world/NoFallModule.kt
@@ -1,22 +1,18 @@
-package com.retrivedmods.wclient.game.module.motion
+package com.retrivedmods.wclient.game.module.misc
 
 import com.retrivedmods.wclient.game.InterceptablePacket
 import com.retrivedmods.wclient.game.Module
 import com.retrivedmods.wclient.game.ModuleCategory
 import org.cloudburstmc.protocol.bedrock.packet.PlayerAuthInputPacket
-import org.cloudburstmc.protocol.bedrock.packet.MovePlayerPacket
 
-class NoFallModule : Module("NoFall", ModuleCategory.Motion) {
+class NoFallModule : Module("no_fall", ModuleCategory.Misc) {
 
     override fun beforePacketBound(interceptablePacket: InterceptablePacket) {
-        if (!isEnabled || !isSessionCreated) return
-        
         val packet = interceptablePacket.packet
         if (packet is PlayerAuthInputPacket) {
-            // Sunucuya sürekli yerdeymişiz bilgisi göndererek düşme hasarını engeller
-            packet.motion = packet.motion?.run { this }
-        } else if (packet is MovePlayerPacket) {
-            packet.isOnGround = true
+            if (session.localPlayer.isOnGround == false) {
+                packet.onGround = true
+            }
         }
     }
 }
